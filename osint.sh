b136bd345a249d2373fdc8fefe6b89ba89352671
Caden F <121118774+MysticalMike60t@users.noreply.github.com>
2026-02-25 07:48:34 -0700

    Update osint.sh
    Signed-off-by: Caden F <121118774+MysticalMike60t@users.noreply.github.com>

--- osint.sh
+++ osint.sh
@@ -1,15 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ ! $SHELL = "bash" ]; then
-    echo "You need to use bash to run this script. You are on $SHELL."
-    return 1
+export PATH="$PATH"
+
+if [ -z "${BASH_VERSION:-}" ]; then
+    if ! exec /usr/bin/env bash "$0" "$@"; then
+        echo "You need to use bash to run this script."
+        exit 1
+    fi
 fi
 
 # ----------------------------------------- #
 #               Bash env init               #
 # ----------------------------------------- #
 
-set -ebhm
+set -ebhmuo pipefail
 
 # ----------------------------------------- #
 #             End Bash env init             #
@@ -32,14 +36,29 @@ fi
 #         Entry Variable Definitions        #
 # ----------------------------------------- #
 
+# Static strings
 readonly TITLE="OSINT.sh"
+
+# Version Definitions
+readonly BLACKBIRD_PYTHON_VERSION=3.14.3
+
+# OS / System info vars
+ACTUAL_USER="${SUDO_USER:-$USER}"
+
 ERRORS=0
+VERBOSE=false
 LOG_FILE="./osint.sh.log"
+LOCAL_INSTALL_BIN="/home/$ACTUAL_USER/.local/bin"
+BLACKBIRD_INSTALL_DIR="/home/$ACTUAL_USER/.local/share/blackbird"
 # Initial arg variable values
 SCRIPT_ARGS_CUSTOM_LOG_OUTPUT_ENABLED=false
 SCRIPT_ARGS_CUSTOM_LOG_OUTPUT_LOCATION=$LOG_FILE
 SCRIPT_ARGS_VERBOSE_ENABLED=false
 SCRIPT_ARGS_NO_INSTALL_ENABLED=false
+SCRIPT_ARGS_BLACKBIRD_INSTALL_DIR=$BLACKBIRD_INSTALL_DIR
+SCRIPT_ARGS_LOCAL_INSTALL_BIN=$LOCAL_INSTALL_BIN
+
+export PATH="$LOCAL_INSTALL_BIN:$PATH"
 
 # ----------------------------------------- #
 #       End Entry Variable Definitions      #
@@ -51,9 +70,13 @@ SCRIPT_ARGS_NO_INSTALL_ENABLED=false
 
 GUM_CHOOSE_STYLE=""
 
+run_as_user() {
+    runuser --user $ACTUAL_USER -- bash -lic "$*"
+}
+
 command_success() {
     local command_output
-    command_output=$1
+    [[ ! -z ${1:-} ]] && command_output=$1
     if [[ $? -ne 0 || $command_output == *"string"* || $command_output == *"strong"* ]]; then
         return 1
     else
@@ -62,7 +85,7 @@ command_success() {
 }
 
 command_exists() {
-    if command -v $1 > /dev/null; then
+    if command -v "$1" > /dev/null; then
         return 0
     else
         return 1
@@ -71,8 +94,8 @@ command_exists() {
 
 package_manager() {
     local TYPE PACKAGE PACKAGE_MANAGER
-    TYPE=$1
-    PACKAGE=$2
+    [[ ! -z ${1:-} ]] && TYPE=$1
+    [[ -n $2 ]] && PACKAGE=$2
 
     check() {
         if command_exists apt; then
@@ -112,7 +135,7 @@ package_manager() {
     }
     clean() {
         local TYPE
-        TYPE=$1
+        [[ ! -z ${1:-} ]] && TYPE=$1
 
         case $TYPE in
             savespace)
@@ -216,33 +239,146 @@ package_manager() {
 # ----------------------------------------- #
 
 use_python() {
-    local TYPE
-    TYPE=$1
+    local TYPE FILENAME REST
+    [[ ! -z ${1:-} ]] && TYPE=$1
+    [[ ! -z ${2:-} ]] && FILENAME=$2
+    shift 2
+    [[ ! -z ${*:-} ]] && REST=$*
+
+    if command_exists pyenv; then
+        export PATH="/home/$ACTUAL_USER/.pyenv/bin:$PATH"
+        eval "$(run_as_user pyenv init - bash)"
+        export PYENV_INSTALLER_SKIP_PROMPTS=1
+    fi
+
+    create_env() {
+        if command_exists pyenv; then
+            run_as_user pyenv install -s 3.14.3
+
+            local PYTHON_BIN="/home/$ACTUAL_USER/.pyenv/versions/3.14.3/bin/python"
+
+            if [[ ! -x "$PYTHON_BIN" ]]; then
+                echo "Python version not installed via pyenv"
+                return 1
+            fi
+
+            "$PYTHON_BIN" -m venv .venv
+        else
+            if command_exists python3; then
+                python3 -m venv .venv
+            else
+                python -m venv .venv
+            fi
+        fi
+    }
+    run_script() {
+        local OPTIONS
+        [[ ! -z ${1:-} ]] && OPTIONS=$("./$1")
+        shift 1
+        [[ ! -z ${*:-} ]] && OPTIONS=$("./$1" "$@")
+
+        if command_exists pyenv; then
+            run_as_user pyenv exec python $OPTIONS
+        else
+            if command_exists python3; then
+                python3 $OPTIONS
+            elif command_exists python; then
+                python $OPTIONS
+            fi
+        fi
+    }
 
     case $TYPE in
         createenv)
-            if command_exists pyenv; then
-                pyenv local 3
-                pyenv exec -m python venv .venv 
+            create_env
+            ;;
+        run)
+            create_env
+            if [[ -n $REST ]]; then
+                run_script $FILENAME $REST
             else
-                if command_exists python3; then
-                    python3 -m venv .venv
-                elif command_exists python; then
-                    python -m venv .venv
-                fi
+                run_script $FILENAME
             fi
             ;;
     esac
 }
 
-start_blackbird() {
-    local TYPE INSTALL_DIR
-    TYPE=$1
-    INSTALL_DIR="$HOME/.local/share/blackbird"
+run_gum() {
+    local TYPE OPTIONS
+    [[ ! -z ${1:-} ]] && TYPE=$1
+    shift 1
+    [[ ! -z ${*:-} ]] && OPTIONS=$*
 
     confirm() {
-        gum confirm && return 0 || return 1
+        if command_exists gum; then
+            if [[ ! -z ${1:-} ]]; then
+                gum confirm "$@" && return 0 || return 1
+            # else
+                gum confirm && return 0 || return 1
+            fi
+        # else
+            # return 1
+        fi
     }
+    choose() {
+        if command_exists gum; then
+            if [[ ! -z ${*:-} ]]; then
+                local RESTARGS
+                RESTARGS=$*
+                gum choose $GUM_CHOOSE_STYLE $RESTARGS && return 0 || return 1
+            # else
+                # return 1
+            fi
+        # else
+            # return 1
+        fi
+    }
+
+    case $TYPE in
+        confirm)
+            confirm "${OPTIONS[@]}"
+            ;;
+        choose)
+            choose $OPTIONS
+            ;;
+    esac
+}
+
+install_blackbird() {
+    while true; do
+        if ! command_exists git; then
+            install pkgmgr git
+            continue
+        fi
+        local current_dir
+        current_dir=$(pwd)
+        rm -rf $BLACKBIRD_INSTALL_DIR
+        rm -f $LOCAL_INSTALL_BIN/blackbird
+        rm -f /etc/environment.d/99-blackbird.conf
+        if $VERBOSE; then
+            mkdir -pv "$BLACKBIRD_INSTALL_DIR" "$LOCAL_INSTALL_BIN"
+            git -v clone https://github.com/p1ngul1n0/blackbird "$BLACKBIRD_INSTALL_DIR"
+        else
+            mkdir -p "$BLACKBIRD_INSTALL_DIR" "$LOCAL_INSTALL_BIN"
+            git clone https://github.com/p1ngul1n0/blackbird "$BLACKBIRD_INSTALL_DIR"
+        fi
+        cd $BLACKBIRD_INSTALL_DIR
+        use_python createenv
+        source "$BLACKBIRD_INSTALL_DIR/.venv/bin/activate" && pip install -r requirements.txt
+        printf "PATH=\"%s:\$PATH\"" $BLACKBIRD_INSTALL_DIR:$LOCAL_INSTALL_BIN >> /etc/environment.d/99-blackbird.conf
+        printf "#!/usr/bin/env bash\nset -euo pipefail\ncurrent_dir=\$(pwd)\nWRKDIR=\"%s\"\nVENV=\"\$WRKDIR/.venv\"\nSCRIPT=\"\$WRKDIR/blackbird.py\"\nif [[ ! -d \"\$VENV\" ]]; then\necho \"Error: Virtual environment not found at \$VENV\"\nexit 1\nfi\nsource \"\$VENV/bin/activate\"\ncd \"\$WRKDIR\"\npython \"\$SCRIPT\" \"\$@\"\ndeactivate\ncd \$current_dir" $BLACKBIRD_INSTALL_DIR >> $LOCAL_INSTALL_BIN/blackbird
+        chown $ACTUAL_USER:$ACTUAL_USER $LOCAL_INSTALL_BIN/blackbird
+        chmod +x $LOCAL_INSTALL_BIN/blackbird
+        cd $current_dir
+
+        return 0
+    done
+}
+
+start_blackbird() {
+    local TYPE INSTALL_DIR
+    [[ ! -z ${1:-} ]] && TYPE=$1
+    INSTALL_DIR=$BLACKBIRD_INSTALL_DIR
     type() {
         case "$TYPE" in
             "username")
@@ -258,7 +394,7 @@ start_blackbird() {
         esac
     }
     output() {
-        if gum confirm "Specify Output?"; then
+        if run_gum confirm "Specify Output?"; then
             local choice
             choice=$(gum choose $GUM_CHOOSE_STYLE "csv" "pdf" "json" "cancel")
             case "$choice" in
@@ -271,27 +407,33 @@ start_blackbird() {
         fi
     }
     verbose() {
-        if gum confirm "Run verbose?"; then
+        if run_gum confirm "Run verbose?"; then
             echo "--verbose"
         fi
     }
     proxy() {
-        if gum confirm "Use proxy?"; then
+        if run_gum confirm "Use proxy?"; then
             local value
             value=$(gum input --placeholder "Proxy URL?")
             echo "--proxy $value"
         fi
     }
     nsfw() {
-        if gum confirm "Remove NSFW results?"; then
+        if run_gum confirm "Remove NSFW results?"; then
             echo "--no-nsfw"
         fi
     }
     extra_params() {
-        if gum confirm "Add extra parameters?"; then
+        if run_gum confirm "Add extra parameters?"; then
             gum input --placeholder "Enter extra parameters"
         fi
     }
+
+    if [[ ! -d $INSTALL_DIR ]] || [[ ! -f $LOCAL_INSTALL_BIN/blackbird ]] || [[ ! -f /etc/environment.d/99-blackbird.conf ]]; then
+        if ! install_blackbird; then
+            printf "Could not install blackbird..."
+        fi
+    fi
 
     local TYPE_ARG OUTPUT_ARG VERBOSE_ARG PROXY_ARG NSFW_ARG EXTRA_ARG
     TYPE_ARG=$(type)
@@ -342,7 +484,31 @@ insane_verbose() {
     
     verbose
     var_details $ERRORS
-    var_details 
+}
+
+install() {
+    local NAME TYPE REST
+    NAME=$1
+    TYPE=$2
+    shift 2
+    REST=$*
+
+    case $TYPE in
+        pkgmgr)
+            if [[ -n $REST ]]; then
+                package_manager $NAME $REST
+            else
+                package_manager $NAME
+            fi
+            ;;
+        custom)
+            case $NAME in
+                blackbird)
+                    install_blackbird
+                    ;;
+            esac
+            ;;
+    esac
 }
 
 # ----------------------------------------- #
@@ -370,7 +536,7 @@ while [ $# -gt 0 ]; do
 
         if [ "$SCRIPT_ARGS_LOG_ENABLED" = true ]; then
             SCRIPT_ARGS_CUSTOM_LOG_OUTPUT_LOCATION="$2"
-            LOG_FILE="$SCRIPT_ARGS_CUSTOM_LOG_OUTPUT_LOCATION"
+            LOG_FILE="$2"
         fi
 
         shift 2
@@ -378,6 +544,7 @@ while [ $# -gt 0 ]; do
         ;;
     -v | --verbose)
         SCRIPT_ARGS_VERBOSE_ENABLED=true
+        VERBOSE=true
         verbose
         ;;
     --insane-verbose)
@@ -386,6 +553,13 @@ while [ $# -gt 0 ]; do
         ;;
     -n | --no-install)
         SCRIPT_ARGS_NO_INSTALL_ENABLED=true
+        ;;
+    --local-install-bin)
+        SCRIPT_ARGS_LOCAL_INSTALL_BIN=$2
+        ;;
+    --blackbird-dir)
+        SCRIPT_ARGS_BLACKBIRD_INSTALL_DIR=$2
+        BLACKBIRD_INSTALL_DIR=$2
         ;;
     *)
         echo "Invalid option: $1" >&2
@@ -406,9 +580,10 @@ done
 
 username_menu() {
     while true; do
-        if command -v gum > /dev/null; then
-            local choice
-            choice=$(gum choose $GUM_CHOOSE_STYLE "Blackbird" "Back")
+        if command_exists gum; then
+            local choice options
+            options=("Blackbird" "Back")
+            choice=$(gum choose $GUM_CHOOSE_STYLE "${options[@]}")
             clear
             case $choice in
                 "Blackbird") start_blackbird "username" ;;
@@ -420,9 +595,10 @@ username_menu() {
 
 email_menu() {
     while true; do
-        if command -v gum > /dev/null; then
-            local choice
-            choice=$(gum choose $GUM_CHOOSE_STYLE "Blackbird" "Back")
+        if command_exists gum; then
+            local choice options
+            options=("Blackbird" "Back")
+            choice=$(run_gum choose $GUM_CHOOSE_STYLE "${options[@]}")
             clear
             case $choice in
                 "Blackbird") start_blackbird "email" ;;
@@ -434,9 +610,10 @@ email_menu() {
 
 main_menu() {
     while true; do
-        if command -v gum > /dev/null; then
-            local choice
-            choice=$(gum choose $GUM_CHOOSE_STYLE "Username" "Email" "Exit")
+        if command_exists gum; then
+            local choice options
+            local options=("Username" "Email" "Exit")
+            choice=$(run_gum choose $GUM_CHOOSE_STYLE "${options[@]}")
             clear
             case $choice in
                 "Username") username_menu ;;
@@ -444,38 +621,36 @@ main_menu() {
                 "Exit") clear && exit ;;
             esac
         else
-            check() {
+            install_gum() {
                 case $PACKAGE_MANAGER in
                     apt)
                         mkdir -p /etc/apt/keyrings
                         curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
                         echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
-                        package_manager update
-                        package_manager install gum
+                        install pkgmgr gum
                         ;;
                     yum)
-                        yum install gum
+                        install pkgmgr gum
                         ;;
                     zypper)
-                        zypper refresh
-                        zypper install gum
+                        install pkgmgr gum
                         ;;
                     dnf)
-                        dnf install gum
+                        install pkgmgr gum
                         ;;
                     nix-env)
-                        nix-env -iA nixpkgs.gum
+                        install pkgmgr nixpkgs.gum
                         ;;
                     pacman)
-                        pacman -S gum
+                        install pkgmgr gum
                         ;;
                     flox)
-                        flox install gum
+                        install pkgmgr gum
                         ;;
                     *)
-                        if command -v brew > /dev/null; then
+                        if command_exists brew; then
                             brew install gum
-                        elif command -v go > /dev/null; then
+                        elif command_exists go; then
                             go install github.com/charmbracelet/gum@latest
                         else
                             echo "error can't install gum :("
@@ -487,10 +662,11 @@ main_menu() {
             }
             echo "Gum is not installed. You need it to use this program."
             echo "Do you want to install it?"
-            select yn in "Yes" "No"; do
-                case $yn in
-                    Yes ) check ; break ;;
-                    No ) exit ;;
+            select strictreply in "Yes" "No"; do
+                relaxedreply=${strictreply:-$REPLY}
+                case $relaxedreply in
+                    Yes | yes | y ) install_gum ; break ;;
+                    No  | no  | n ) break ;;
                 esac
             done
         fi
@@ -505,7 +681,7 @@ main_menu() {
 #                   Init                    #
 # ----------------------------------------- #
 
-package_manager install git
+clear
 
 main_menu
 
